25ccac0912aaa294327140cfccdd80b04482496d
Trong Hieu <tronghieu60s@gmail.com>
2021-05-15 20:11:20 -0700

    Initial Commit

---
+++ Youtube Music/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube Music
+//
+//  Created by Trong Hieu on 5/15/21.
+//  Copyright © 2021 Trong Hieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Youtube MusicTests/Youtube_MusicTests.swift
@@ -0,0 +1,34 @@
+//
+//  Youtube_MusicTests.swift
+//  Youtube MusicTests
+//
+//  Created by Trong Hieu on 5/15/21.
+//  Copyright © 2021 Trong Hieu. All rights reserved.
+//
+
+import XCTest
+@testable import Youtube_Music
+
+class Youtube_MusicTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
