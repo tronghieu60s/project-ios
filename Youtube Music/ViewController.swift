2a74aab096d6980b0a3cfdd3703e4e7d57e8a17c
Trong Hieu <tronghieu60s@gmail.com>
2021-05-17 08:02:08 -0700

    first commit

--- Youtube Music/ViewController.swift
+++ Youtube Music/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnStart: UIButton!
+    
+    @IBAction func btnStartAction(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        btnStart.layer.cornerRadius = 10
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let alert:UIAlertController = UIAlertController(title: "Đăng Nhập", message: "Vui lòng đăng nhập để tiếp tục", preferredStyle: .alert)
+        
+        alert.addTextField { (txtUsername) in
+            txtUsername.placeholder = "Nhập tên người dùng vào đây..."
+        }
+        
+        alert.addTextField { (txtPassword) in
+            txtPassword.placeholder = "Nhập mật khẩu vào đây..."
+        }
+        
+        let btnLogin:UIAlertAction = UIAlertAction(title: "Đăng nhập", style: .default) { (btnLogin) in
+            
+        }
+        
+        let btnCancel:UIAlertAction = UIAlertAction(title: "Huy", style: .cancel) { (btnCancel) in
+            
+        }
+        
+        alert.addAction(btnLogin)
+        alert.addAction(btnCancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
-
