6e138e427d273c5c1d3eaab7720c9b32d5392007
Trong Hieu <tronghieu60s@gmail.com>
2021-05-20 03:36:46 -0700

    firebase config, get data from firebase

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # project-ios
 A Project Swift IOS - YouTube Music
+
+
+## Firebase
+
+For install firebase, before you need a account, and create a new firebase database realtime.
+
+### Install Cocoapods
+
+```bash
+$ sudo gem install cocoapods -v 1.8.4
+```
+
+### Installation
+
+Install And Usage: https://www.youtube.com/watch?v=tpsffoRh9u0.
+
+Steps:
+
+
+```bash
+$ pod init
+```
+
+Just Install  ```Pod "Firebase/Database"```. (Pass To PodFile)
+
+```bash
+$ pod install
+```
+
+Open File .xcworkspace By Xcode To Run.
+
+### Fix Error When First Run Firebase
+
+Error: Double-quoted include in framework header expected.
+
+Fix: https://stackoverflow.com/a/63979662/9724078
--- Youtube Music/ViewController.swift
+++ Youtube Music/ViewController.swift
@@ -6,18 +6,25 @@
 //  Copyright © 2021 Trong Hieu. All rights reserved.
 //
 
+import FirebaseDatabase
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var database: DatabaseReference!
 
     @IBOutlet weak var btnStart: UIButton!
     
     @IBAction func btnStartAction(_ sender: Any) {
-        
+        database.observe(DataEventType.value, with: { (snapshot) in
+            let value = snapshot.value as? [String : AnyObject] ?? [:]
+            print("Value: \(value)")
+        })
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        database = Database.database().reference()
         // Do any additional setup after loading the view.
         btnStart.layer.cornerRadius = 10
     }
@@ -31,14 +38,19 @@ class ViewController: UIViewController {
         
         alert.addTextField { (txtPassword) in
             txtPassword.placeholder = "Nhập mật khẩu vào đây..."
+            txtPassword.isSecureTextEntry = true
         }
         
         let btnLogin:UIAlertAction = UIAlertAction(title: "Đăng nhập", style: .default) { (btnLogin) in
-            
+            let email: String = alert.textFields![0].text!
+            let password: String = alert.textFields![1].text!
+            if(email == "tronghieu60s@gmail.com" && password == "123456"){
+                
+            }
         }
         
         let btnCancel:UIAlertAction = UIAlertAction(title: "Huy", style: .cancel) { (btnCancel) in
-            
+            print("huy dang nhap")
         }
         
         alert.addAction(btnLogin)
